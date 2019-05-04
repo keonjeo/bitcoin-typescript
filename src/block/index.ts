2d6dc0f7a7670bb56d2b20b7a0dc811e661d8844
Tim <lins15@vanke.com>
2019-05-04 16:55:42 +0800

    fix(constants): rename constants

--- src/block/index.ts
+++ src/block/index.ts
@@ -2,7 +2,7 @@ import sha256 from 'crypto-js/sha256'
 import hmacSHA512 from 'crypto-js/hmac-sha512'
 import Base64 from 'crypto-js/enc-base64'
 
-import DIFFICULTY from '../contants/difficulty'
+import DIFFICULTY from '../constants/difficulty'
 
 interface Block {
   Index: number
