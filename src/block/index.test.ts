22b76ccc5630a81b5e016a7ea0a075b252027b90
Tim <lins15@vanke.com>
2019-05-04 23:18:50 +0800

    feat(add @types pkgs): update ts

--- src/block/index.test.ts
+++ src/block/index.test.ts
@@ -1,18 +1,19 @@
 import { CalculateHash } from './index'
 
-describe('test CalculateHash func cryto', () => {
-  it('CalculateHash sha256 cryto', () => {
+describe('test CalculateHash func crypto', () => {
+  it('CalculateHash sha256 crypto', () => {
     const blockMessage = {
       Index: 1,
       Timestamp: 234343434343434,
       Content: '23232',
       Hash: 'fafafa',
-      PreHash: 'fljfljfjlajl',
+      PreHash:
+        '2c467b686c1da17bda6a67ab20172b44a248174d5dcd34856e83a80f5697425a',
       Difficulty: 2,
       Nonce: '232323'
     }
     expect(CalculateHash(blockMessage)).toEqual(
-      '5c467b686c1da17bda6a27ab20172b44a248174d5dcd86856e83a80f5697425a'
+      'add07737c809c8b37ab659c31ebf1123442f00c4343963a5ae4e9f110f3c1ab8'
     )
   })
 })
--- src/block/index.ts
+++ src/block/index.ts
@@ -1,8 +1,8 @@
+import { SHA256 } from 'crypto-js'
 // import hmacSHA512 from 'crypto-js/hmac-sha512'
 // import Base64 from 'crypto-js/enc-base64'
 
 // import DIFFICULTY from '../constants/difficulty'
-const sha256 = require('crypto-js/sha256')
 interface Block {
   Index: number
   Timestamp: number
@@ -15,7 +15,7 @@ interface Block {
 
 export function CalculateHash(block: Block): string {
   const { Index, Timestamp, Content, Hash, PreHash, Difficulty, Nonce } = block
-  return sha256(
+  return SHA256(
     `${Index}${Timestamp}${Content}${Hash}${PreHash}${Difficulty}${Nonce}`
   ).toString()
 }
