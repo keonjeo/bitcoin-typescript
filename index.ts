2ec406c9954cace274384deb472efb4eeb52faa6
Tim <lins15@vanke.com>
2019-05-04 17:03:22 +0800

    update index

--- index.ts
+++ index.ts
@@ -1,13 +1,1 @@
 import { CalculateHash } from './src/block'
-
-console.log(
-  CalculateHash({
-    Index: 1,
-    Timestamp: '23232',
-    Content: '23232',
-    Hash: 'fafafa',
-    PreHash: 'fljfljfjlajl',
-    Difficulty: 2,
-    Nonce: '232323'
-  })
-)
